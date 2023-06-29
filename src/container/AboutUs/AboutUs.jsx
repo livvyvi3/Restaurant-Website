6b74c26445f6d3bd8a4e7897bdd42186ac3480e0
livvyvi3 <machinguravongai@gmail.com>
2023-06-29 22:18:44 +0200

    adding about us component

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import './AboutUs.css';
+import { images } from "../../constants";
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="g Letter" />
+    </div>
+
+    <div className="app__aboutus-content flex_center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt='about_spoon' className="spoon__img"/>
+        <p className="p__opensans"></p>
+      </div>
+    </div>
   </div>
 );
 
