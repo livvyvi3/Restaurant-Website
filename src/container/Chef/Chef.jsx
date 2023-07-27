2b2e0db8cebdd60380225674e4f6c6fbd7971247
livvyvi3 <machinguravongai@gmail.com>
2023-07-27 23:24:21 +0200

    chef menu

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -19,9 +19,14 @@ const Chef = () => (
           <img src={images.quote} alt='quote'/>
           <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit .</p>
         </div>
-        <p className="opensans">
+        <p className="p__opensans">
           auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu. Congue iaculis integer curabitur semper sit nunc.
         </p>
+      </div>
+      <div className="app__chef-sign">
+        <p>Kevin Lou</p>
+        <p className="p__opensans">Chef &amp; Co-Founder</p>
+        <img src={images.sign} alt="sign"/>
       </div>
     </div>
   </div>
