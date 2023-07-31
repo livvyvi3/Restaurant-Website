776e89b91a7cd52fa6e39ddf2ee87869f6c4048f
livvyvi3 <machinguravongai@gmail.com>
2023-07-31 23:58:10 +0200

    created video section

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import {BsFillPlayFill, BsPauseFill} from 'react'
 
+import {meal} from '../../constants'
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  
+  return (
+    <div className="app__video">
+      <video
+        src={meal}
+        ref={vidref}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+    </div>
+  )
+};
 
 export default Intro;
