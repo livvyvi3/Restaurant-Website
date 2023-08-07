472e67471f001da446524ec922022d087f715468
livvyvi3 <machinguravongai@gmail.com>
2023-08-07 17:35:28 +0200

    styling to awards

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -5,7 +5,7 @@ import {images, data} from '../../constants';
 import './Laurels.css';
 
 const AwardCard = ({award: {imgUrl, title, subtitle}}) =>(
-  <div className="app__laurels_awars-card">
+  <div className="app__laurels_awards-card">
     <img src={imgUrl} alt="award" />
     <div className="app__laurels_awards-card_content">
       <p className="p__cormorant" style={{color: '#DCCA87'}}>{title}</p>
@@ -16,7 +16,7 @@ const AwardCard = ({award: {imgUrl, title, subtitle}}) =>(
 )
 
 const Laurels = () => (
-  <div className="app__bg app__wrappeer section__padding" id='awards'>
+  <div className="app__bg app__wrapper section__padding" id='awards'>
     <div className="app__wrapper_info">
       <SubHeading title="Awards &amp; recognition"/>
       <h1 className="headtext__cormorant">Our Laurels</h1>
